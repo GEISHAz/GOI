37c529e27aace25c59c248340a213d52fbe890fd
이귀현 <dlrnlgus7660@naver.com>
2024-03-12 08:54:33 +0900

    feat: front-end 프로젝트 구조 설정

--- [FE]front-end/dokkaebi/src/pages/main/index.jsx
+++ [FE]front-end/dokkaebi/src/pages/main/index.jsx
@@ -1,8 +1,19 @@
-import styles from '../../images/main/background.gif';
+import Background from '../../images/main/background.gif';
 
 export default function Main() {
+  const backgroundStyle = {
+    backgroundImage: `url(${Background})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    width: '100%',
+    height: '100%',
+    position: 'fixed',
+    top: 0,
+    left: 0,
+  };
+
   return (
-    <div className={styles.body}>
+    <div style={ backgroundStyle }>
       <h1>메인페이지</h1>
     </div>
   );
