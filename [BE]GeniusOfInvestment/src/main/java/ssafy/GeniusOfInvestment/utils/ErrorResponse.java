c2ea7644910b579e5c6b3aed2b25888b7ec7b669
정원준 <museoul@naver.com>
2024-03-11 17:12:50 +0900

    Feat: Spring 기본 설정 완료

---
+++ [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/utils/ErrorResponse.java
@@ -0,0 +1,17 @@
+package ssafy.GeniusOfInvestment.utils;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class ErrorResponse {
+
+    private String message;
+    private String errorType;
+    private String fieldName;
+
+
+}
---
+++ [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/utils/UserRole.java
@@ -0,0 +1,24 @@
+package ssafy.GeniusOfInvestment.utils;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum UserRole implements GrantedAuthority {
+    USER("ROLE_USER","유저권한"),
+    ADMIN("ROLE_ADMIN", "관리자권한");
+
+    private String authority;
+    private String description;
+
+    UserRole(String authority, String description){
+        this.authority = authority;
+        this.description = description;
+    }
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+}
