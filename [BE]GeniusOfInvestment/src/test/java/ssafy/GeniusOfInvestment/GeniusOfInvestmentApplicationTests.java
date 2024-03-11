78d0eb8695b25d011ba49f717fbd3e6ccff92b36
정원준 <museoul@naver.com>
2024-03-11 17:43:55 +0900

    Feat: 프로젝트 설정 파일 추가

---
+++ [BE]GeniusOfInvestment/src/test/java/ssafy/GeniusOfInvestment/GeniusOfInvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ssafy.GeniusOfInvestment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeniusOfInvestmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
