603d72c9d0232209872c9d3a31aba4ce2018d775
정원준 <museoul@naver.com>
2024-03-12 16:18:19 +0900

    Feat: 엔티티 작성 완료

---
+++ [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/entity/Channel.java
@@ -0,0 +1,36 @@
+package ssafy.GeniusOfInvestment.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Channel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "channel_id", columnDefinition = "INT UNSIGNED")
+    private Long id;
+
+    private String chName;
+
+    //-----------------------------------------------------
+    @OneToMany(mappedBy = "channel", cascade = CascadeType.ALL)
+    private List<User> participants = new ArrayList<>();
+
+    @OneToMany(mappedBy = "channel", cascade = CascadeType.ALL)
+    private List<Room> rooms = new ArrayList<>();
+
+    public void addRooms(Room room){
+        rooms.add(room);
+        room.setChannel(this);
+    }
+    public void addParticipants(User user){
+        participants.add(user);
+        user.setChannel(this);
+    }
+}
---
+++ [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/entity/Room.java
@@ -0,0 +1,56 @@
+package ssafy.GeniusOfInvestment.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "room_id", columnDefinition = "INT UNSIGNED")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "channel_id")
+    private Channel channel;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String password;
+
+    //0이면 비밀방, 1이면 공개방
+    @Column(columnDefinition = "TINYINT(1) default 1")
+    private boolean isPublic;
+
+    @ColumnDefault("1")
+    private int curNum;
+
+    //0이면 대기, 1이면 게임중, 2이면 없어진 방
+    @Column(columnDefinition = "TINYINT(2) default 0")
+    private int status;
+
+    private int fromYear;
+
+    private int endYear;
+
+    private int turnNum;
+
+    //새로운 엔티티가 저장(추가)되기 직전에
+    @PrePersist
+    protected void onCreate() {
+        turnNum = endYear - fromYear + 1;
+    }
+
+    //기존의 엔티티가 업데이트되기 직전에
+//    @PreUpdate
+//    protected void onUpdate() {
+//
+//    }
+}
