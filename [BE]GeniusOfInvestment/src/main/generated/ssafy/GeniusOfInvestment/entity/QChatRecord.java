40438847ae5e6f1aac476d6a26787baa2d7d5b56
jungminho <allmin9702@naver.com>
2024-03-14 02:22:37 +0900

    Feat : Dockerfilãe 생성

---
+++ [BE]GeniusOfInvestment/src/main/generated/ssafy/GeniusOfInvestment/entity/QChatRecord.java
@@ -0,0 +1,53 @@
+package ssafy.GeniusOfInvestment.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QChatRecord is a Querydsl query type for ChatRecord
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QChatRecord extends EntityPathBase<ChatRecord> {
+
+    private static final long serialVersionUID = 775204993L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QChatRecord chatRecord = new QChatRecord("chatRecord");
+
+    public final QFriend chatId;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath msg = createString("msg");
+
+    public QChatRecord(String variable) {
+        this(ChatRecord.class, forVariable(variable), INITS);
+    }
+
+    public QChatRecord(Path<? extends ChatRecord> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QChatRecord(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QChatRecord(PathMetadata metadata, PathInits inits) {
+        this(ChatRecord.class, metadata, inits);
+    }
+
+    public QChatRecord(Class<? extends ChatRecord> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.chatId = inits.isInitialized("chatId") ? new QFriend(forProperty("chatId"), inits.get("chatId")) : null;
+    }
+
+}
+
