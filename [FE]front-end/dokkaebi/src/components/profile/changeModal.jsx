605c32e3f0165b8db1561cc889a73fc17a623e45
이귀현 <dlrnlgus7660@naver.com>
2024-03-15 16:04:08 +0900

    Feat: 프로필 페이지 UI 구현 및 사진 변경 모달 구현중

--- [FE]front-end/dokkaebi/src/components/profile/changeModal.jsx
+++ [FE]front-end/dokkaebi/src/components/profile/changeModal.jsx
@@ -7,13 +7,13 @@ import green from '../../images/signUp/green.gif';
 import yellow from '../../images/signUp/yellow.gif';
 import pink from '../../images/signUp/pink.gif';
 import orange from '../../images/signUp/orange.gif';
-import styles from './kakaoLogin.module.css'
+import styles from './changeModal.module.css';
 
-export default function choiceModal({ onClose }) {
-  const [selectedImage, setSelectedImage] = useState('');
+export default function ChangeModal({ onClose }) {
   const dispatch = useDispatch();
+  const [selectedImage, setSelectedImage] = useState('');
 
-  // 선택된 사진 -> dispatch하여 store에 저장
+  // 이미지 선택 핸들러
   const handleImageSelect = (image) => {
     setSelectedImage(image);
     dispatch(setUserProfileImage(image));
@@ -22,44 +22,33 @@ export default function choiceModal({ onClose }) {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
       <div className="bg-black/85 p-5 rounded-lg w-1/2">
-        {/* 모달 헤더 */}
-        <h1 className="text-3xl text-center text-white mb-10 font-bold">도깨비 변경</h1>        
+        <h1 className="text-3xl text-center text-white mb-10 font-bold">프로필 이미지 변경</h1>        
         <div className="flex justify-between items-center mb-5">
-          {/* 왼쪽 미리보기 영역 */}
           <div className="flex-1 flex flex-col items-center justify-center">
             <p className="text-white mb-4 font-bold">미리보기</p>
             <div className={`${styles.previewBoxModal} bg-white rounded-lg flex justify-center items-center`}>
-              {selectedImage &&
-              <img
-                src={selectedImage}
-                alt="미리보기"
-                className="max-w-full max-h-full"/>}
+              {selectedImage && <img src={selectedImage} alt="미리보기" className="max-w-full max-h-full"/>}
             </div>
           </div>
           
-          {/* 오른쪽 이미지 선택 영역 */}
-          <div className="flex-1 grid grid-cols-3 gap-0 mr-10">
+          <div className="flex-1 grid grid-cols-3 gap-4 mr-10">
             {[blue, brown, yellow, pink, orange, green].map((image, index) => (
               <img
                 key={index}
                 src={image}
-                alt={`도깨비-${index}`}
+                alt={`프로필 이미지-${index}`}
                 className={`w-full h-32 cursor-pointer border-2 ${selectedImage === image ? 'border-4 border-green-700' : 'border-white'}`}
                 onClick={() => handleImageSelect(image)}/>
             ))}
           </div>
         </div>
 
-        {/* 하단 중앙 "선택" 버튼 */}
-        <div className="flex justify-center text-center mt-10">
-          <button
-            onClick={onClose}
-            className="bg-white w-24 h-10 font-bold text-black rounded"
-          >
-            선 택
+        <div className="flex justify-center mt-10">
+          <button onClick={onClose} className="bg-white w-24 h-10 font-bold text-black rounded">
+            선택
           </button>
         </div>
       </div>
     </div>
-  )
+  );
 }
