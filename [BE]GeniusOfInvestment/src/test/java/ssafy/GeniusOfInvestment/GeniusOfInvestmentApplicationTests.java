db4fab0258e0d8d906d19af9617ed0b03bf5a13f
jungminho <allmin9702@naver.com>
2024-03-15 15:15:37 +0900

    Fix : test코드 수정

--- [BE]GeniusOfInvestment/src/test/java/ssafy/GeniusOfInvestment/GeniusOfInvestmentApplicationTests.java
+++ [BE]GeniusOfInvestment/src/test/java/ssafy/GeniusOfInvestment/GeniusOfInvestmentApplicationTests.java
@@ -3,8 +3,10 @@ package ssafy.GeniusOfInvestment;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class GeniusOfInvestmentApplicationTests {
 
+	@Test
 	void contextLoads() {
 	}
 
