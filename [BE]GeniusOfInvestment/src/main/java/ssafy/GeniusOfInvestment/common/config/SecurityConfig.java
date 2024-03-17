d59c5cfdf8d2e71b1091b7b9d225fbf48f5b5704
이준서 <wnstj7866@naver.com>
2024-03-18 00:24:11 +0900

    Feat : Redis, RestTemplate, Security Config 추가

---
+++ [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/common/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package ssafy.GeniusOfInvestment.common.config;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ssafy.GeniusOfInvestment._common.jwt.JwtAuthorizationFilter;
+import ssafy.GeniusOfInvestment._common.jwt.JwtExceptionFilter;
+import ssafy.GeniusOfInvestment._common.oauth2.HttpCookieOAuth2AuthorizationRequestRepository;
+import ssafy.GeniusOfInvestment._common.oauth2.handler.OAuth2AuthenticationFailureHandler;
+import ssafy.GeniusOfInvestment._common.oauth2.handler.OAuth2AuthenticationSuccessHandler;
+import ssafy.GeniusOfInvestment._common.oauth2.service.CustomOAuth2UserService;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
+    private final HttpCookieOAuth2AuthorizationRequestRepository httpCookieOAuth2AuthorizationRequestRepository;
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+    private final JwtExceptionFilter jwtExceptionFilter;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+//                        .requestMatchers("/api/members/**").authenticated()
+                        .requestMatchers("/api/auth/**").permitAll()
+//                        .requestMatchers(antMatcher("/api"))
+//                        .anyRequest().authenticated())
+                        .anyRequest().permitAll())
+                .sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2Login(configure ->
+                        configure.authorizationEndpoint(config -> config.authorizationRequestRepository(httpCookieOAuth2AuthorizationRequestRepository))
+                                .userInfoEndpoint(config -> config.userService(customOAuth2UserService))
+                                .successHandler(oAuth2AuthenticationSuccessHandler)
+                                .failureHandler(oAuth2AuthenticationFailureHandler)
+                )
+                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtExceptionFilter, JwtAuthorizationFilter.class);
+
+        return http.build();
+    }
+}
