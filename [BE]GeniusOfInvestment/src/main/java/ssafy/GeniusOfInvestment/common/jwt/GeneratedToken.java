66c78152d964d218ecb526f6eeae04ba6caadca2
이준서 <wnstj7866@naver.com>
2024-03-18 00:27:11 +0900

    Feat : JWT 관련 클래스 추가

---
+++ [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/common/jwt/GeneratedToken.java
@@ -0,0 +1,15 @@
+package ssafy.GeniusOfInvestment.common.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@Builder @ToString
+public class GeneratedToken {
+
+    private String accessToken;
+    private String refreshToken;
+}
