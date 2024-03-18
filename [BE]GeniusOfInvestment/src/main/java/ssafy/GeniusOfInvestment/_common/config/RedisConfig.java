d786331ed8dd36ee29023f23d6a754d28d9e00af
정원준 <museoul@naver.com>
2024-03-18 12:13:17 +0900

    Feat: RedisConfig 파일 수정

--- [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/_common/config/RedisConfig.java
+++ [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/_common/config/RedisConfig.java
@@ -2,6 +2,7 @@ package ssafy.GeniusOfInvestment._common.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -9,6 +10,7 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableRedisRepositories
@@ -24,27 +26,24 @@ public class RedisConfig {
     @Value(value = "${redis.password}")
     private String password;
 
+    private final RedisProperties redisProperties;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(host);
-        redisStandaloneConfiguration.setPort(port);
-        redisStandaloneConfiguration.setPassword(password);
+        redisStandaloneConfiguration.setHostName(redisProperties.getHost());
+        redisStandaloneConfiguration.setPort(redisProperties.getPort());
+        redisStandaloneConfiguration.setPassword(redisProperties.getPassword());
         return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
+    // setKeySerializer, setValueSerializer 설정으로 redis-cli를 통해 직접 데이터를 보는게 가능하다.
     @Bean
-    public RedisTemplate<?, ?> redisTemplate() {
-
-        // redisTemplate 를 받아와서 set, get, delete 를 사용
-        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
-
-        /*
-         * setKeySerializer, setValueSerializer 설정
-         * redis-cli 을 통해 직접 데이터를 조회 시 알아볼 수 없는 형태로 출력되는 것을 방지
-         */
+    public RedisTemplate<Object, Object> redisTemplate() {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
         redisTemplate.setConnectionFactory(redisConnectionFactory());
-
         return redisTemplate;
     }
 }
