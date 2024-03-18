b16e31d4256b8490eee05665ee57250059e871c0
wnstj7866 <wnstj7866@naver.com>
2024-03-18 12:26:25 +0900

    Feat : 채널 입장 API 생성

--- [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/user/controller/UserController.java
+++ [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/user/controller/UserController.java
@@ -23,30 +23,37 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("{id}")
-    public SuccessResponse<UserInfoResponseDto> getUser(@PathVariable(value = "id")Long userId){
-        return SuccessResponse.of(SuccessType.GET_USER_INFO_SUCCESSFULLY,userService.getUser(userId));
+    public SuccessResponse<UserInfoResponseDto> getUser(@PathVariable(value = "id") Long userId) {
+        return SuccessResponse.of(SuccessType.GET_USER_INFO_SUCCESSFULLY, userService.getUser(userId));
     }
 
     @PutMapping("/{id}/image-id")
-    public SuccessResponse<Void> updateUserImageId(@PathVariable(value = "id")Long userId, @RequestBody UpdateImageIdRequestDto updateImageIdRequestDto){
-        log.info(updateImageIdRequestDto.getImageId()+"controller");
-        userService.updateUserImageId(userId,updateImageIdRequestDto);
+    public SuccessResponse<Void> updateUserImageId(@PathVariable(value = "id") Long userId,
+            @RequestBody UpdateImageIdRequestDto updateImageIdRequestDto) {
+        log.info(updateImageIdRequestDto.getImageId() + "controller");
+        userService.updateUserImageId(userId, updateImageIdRequestDto);
         return SuccessResponse.from(SuccessType.UPDATE_USER_IMAGE_SUCCESSFULLY);
     }
 
     @PutMapping("/{id}/nick-name")
-    public SuccessResponse<Void> updateUserNickName(@PathVariable(value = "id")Long userId, @RequestBody UpdateNickNameRequestDto updateNickNameRequestDto){
-        userService.updateUserNickName(userId,updateNickNameRequestDto);
+    public SuccessResponse<Void> updateUserNickName(@PathVariable(value = "id") Long userId,
+            @RequestBody UpdateNickNameRequestDto updateNickNameRequestDto) {
+        userService.updateUserNickName(userId, updateNickNameRequestDto);
         return SuccessResponse.from(SuccessType.UPDATE_USER_NICKNAME_SUCCESSFULLY);
     }
 
     @GetMapping("/rank")
-    public SuccessResponse<List<RankInfoResponseDto>> getRankInfo(){
-        return SuccessResponse.of(SuccessType.GET_RANK_INFO_SUCCESSFULLY,userService.getRankInfo());
+    public SuccessResponse<List<RankInfoResponseDto>> getRankInfo() {
+        return SuccessResponse.of(SuccessType.GET_RANK_INFO_SUCCESSFULLY, userService.getRankInfo());
     }
 
     @GetMapping("/{id}/rank")
-    public SuccessResponse<UserRankResponseDto> getRankByExp(@PathVariable(value = "id")Long userId){
-        return SuccessResponse.of(SuccessType.GET_USER_RANK_SUCCESSFULLY,userService.getUserRank(userId));
+    public SuccessResponse<UserRankResponseDto> getRankByExp(@PathVariable(value = "id") Long userId) {
+        return SuccessResponse.of(SuccessType.GET_USER_RANK_SUCCESSFULLY, userService.getUserRank(userId));
+    }
+
+    @GetMapping("/{id}/joinChannel")
+    public SuccessResponse<UserRankResponseDto> joinChannel(@PathVariable(value = "id") Long userId) {
+        return SuccessResponse.of(SuccessType.GET_USER_RANK_SUCCESSFULLY, userService.getUserRank(userId));
     }
 }
