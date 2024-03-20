67d30ca04c1686bef24785b0274ffd7ecf7990fc
wnstj7866 <wnstj7866@naver.com>
2024-03-20 11:19:50 +0900

    Fix : RedisSubscriber 수정

--- [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/_common/stomp/pubsub/RedisSubscriber.java
+++ [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/_common/stomp/pubsub/RedisSubscriber.java
@@ -18,15 +18,15 @@ public class RedisSubscriber {
 
 
     // Redis에서 메시지가 발행(publish)되면 대기하고 있던 Redis Subscriber가 해당 메시지를 받아 처리
-    public void sendMessage(String publishMessage) {
-        log.info("send message to subscriber : {}", publishMessage);
-        try {
-            // dataDTO 객채로 맵핑
-            MessageDto data = objectMapper.readValue(publishMessage, MessageDto.class);
-            log.info("[서버] 메세지를 DTO에 맵핑 : {}", data.getType());
-
-            if (MessageType.CHANNEL_ENTER.equals(data.getType())) {
-                log.info("");
-            }
-        }
+//    public void sendMessage(String publishMessage) {
+//        log.info("send message to subscriber : {}", publishMessage);
+//        try {
+//            // dataDTO 객채로 맵핑
+//            MessageDto data = objectMapper.readValue(publishMessage, MessageDto.class);
+//            log.info("[서버] 메세지를 DTO에 맵핑 : {}", data.getType());
+//
+//            if (MessageType.CHANNEL_ENTER.equals(data.getType())) {
+//                log.info("");
+//            }
+//        }
 }
