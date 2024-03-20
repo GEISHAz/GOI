af1c4a2d9a4e4aad040803313b4558af084611c1
이귀현 <dlrnlgus7660@naver.com>
2024-03-20 15:41:00 +0900

    fix : 라우터 수정

--- [FE]front-end/dokkaebi/src/api/accessCheck.js
+++ [FE]front-end/dokkaebi/src/api/accessCheck.js
@@ -4,7 +4,7 @@ import { setIsLogin } from '../features/login/authSlice.js';
 
 // Axios 인스턴스 생성
 const api = axios.create({
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://j10d202.p.ssafy.io/",
 });
 
 // 응답 인터셉터 설정
@@ -14,19 +14,18 @@ api.interceptors.response.use(
     if (error.response && error.response.status === 401) {
       try {
         // 새 액세스 토큰 발급 요청
-        const response = await api.post("api/auth/regenerate-token")
+        const response = await api.post("api/auth/regenerate-token");
         const { accessToken } = response.data;
-        console.log("엑세스 토큰 확인 :", accessToken)
+        console.log("새 액세스 토큰 확인 :", accessToken);
         // 새 액세스 토큰 저장
         localStorage.setItem("accessToken", accessToken);
 
-        // 원래 요청 재시도
+        // 원래 요청에 새 토큰을 적용하여 재시도
         error.config.headers["Authorization"] = `Bearer ${accessToken}`;
-        config.headers.Authorization = `Bearer ${accessToken}`;
-        return api.request(error.config);
+        return api.request(error.config); // 수정된 요청 재시도
       } catch (refreshError) {
         // 새 토큰 발급 실패 시 로그아웃 처리
-        console.error("Unable to refresh token", refreshError);
+        console.error("새 토큰 발급 실패", refreshError);
         store.dispatch(setIsLogin(false));
         localStorage.removeItem("accessToken");
         localStorage.removeItem("refreshToken");
