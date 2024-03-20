d8208c4e3fb24a61c6aa8085d565219066c3b176
류태수 <taesu960625@gmail.com>
2024-03-20 17:23:16 +0900

    Feat : 메인 페이지 백 연결중

---
+++ [FE]front-end/dokkaebi/src/components/gamePlay/mainStock/InvestmentInfo.jsx
@@ -0,0 +1,57 @@
+// InvestmentInfo.js
+
+import React, { useState, useEffect } from "react";
+import styles from "./InvestmentInfo.module.css";
+import StockExchange from "./StockExchange";
+
+export default function InvestmentInfo(props) {
+  const [colorClass, setColorClass] = useState("");
+  const [stockExchangeModal, setStockExchangeModal] = useState(false);
+  const [transactionType, setTransactionType] = useState(""); // 매수 또는 매도 여부 상태
+
+  const stockExchangeModalOpen = (type) => {
+    setTransactionType(type);
+    setStockExchangeModal(true);
+  };
+
+  useEffect(() => {
+    const percent = props.percent;
+
+    // percent 값에 따라 적절한 클래스 설정
+    if (percent > 0) {
+      setColorClass(styles.upDownPercentColorRed);
+    } else if (percent < 0) {
+      setColorClass(styles.upDownPercentColorBlue);
+    } else {
+      setColorClass(styles.upDownPercentColorBlack);
+    }
+  }, [props.percent]);
+
+  return (
+    <div className={styles.investmentInfo}>
+      <div className={styles.bunya}>
+        <p>{props.company}</p>
+      </div>
+      <div className={styles.jusu}>
+        <p>{props.price}/주</p>
+      </div>
+      <div className={`${styles.upDown} ${colorClass}`}>
+        <p>{props.percent}%</p>
+      </div>
+      <div className={styles.buttons}>
+        <button className={styles.buyButtons} onClick={() => stockExchangeModalOpen("buy")}>매수</button>
+        <hr />
+        <button className={styles.sellButtons} onClick={() => stockExchangeModalOpen("sell")}>매도</button>
+      </div>
+      {stockExchangeModal && (
+        <StockExchange
+          setStockExchangeModal={setStockExchangeModal}
+          company={props.company}
+          price={props.price}
+          percent={props.percent}
+          transactionType={transactionType}
+        />
+      )}
+    </div>
+  );
+}
--- [FE]front-end/dokkaebi/src/features/game/gameSlice.js
+++ [FE]front-end/dokkaebi/src/features/game/gameSlice.js
@@ -1,1 +1,24 @@
-// 더미데이터
+import { createSlice } from '@reduxjs/toolkit';
+
+export const gameSlice = createSlice({
+  name : 'auth',
+  initialState : {
+    money : 500000,
+    ready : false,
+    point : 0
+  },
+  reducers : {
+    setMoney : (state,action) => {
+      state.money = state.money + action.payload
+    },
+    setReady : (state,action) => {
+      state.ready = action.payload
+    },
+    setPoint: (state, action) => {
+      state.point = state.point + action.payload;
+    },
+  }
+})
+
+export const { setUserNickname, setUserProfileImage, setIsLogin, setPreviousUserNickname } = authSlice.actions
+export default gameSlice.reducer
