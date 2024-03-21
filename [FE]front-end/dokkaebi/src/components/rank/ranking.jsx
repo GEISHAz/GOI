8ddb3a0de6f742bc7c8588c896ee9292937a8ed9
이귀현 <dlrnlgus7660@naver.com>
2024-03-21 20:15:03 +0900

    Fix: imageId 찾는 함수 수정

--- [FE]front-end/dokkaebi/src/components/rank/ranking.jsx
+++ [FE]front-end/dokkaebi/src/components/rank/ranking.jsx
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import axios from "axios";
+import blue from '../../images/signUp/blue.gif';
+import brown from '../../images/signUp/brown.gif';
+import green from '../../images/signUp/green.gif';
+import yellow from '../../images/signUp/yellow.gif';
+import pink from '../../images/signUp/pink.gif';
+import orange from '../../images/signUp/orange.gif';
 import BackA from '../../images/hub/backA.png';
 import BackB from '../../images/hub/backB.png';
 import styles from './ranking.module.css';
-import { useSelector } from 'react-redux';
-import axios from "axios";
 
 export default function Ranking() {
   const navigate = useNavigate();
@@ -17,19 +23,30 @@ export default function Ranking() {
   const [otherUsers, setOtherUsers] = useState([]);
   const [isHovering, setIsHovering] = useState(false);
 
+  const images = [
+    { id: 1, src: blue, alt: "파랑도깨비" },
+    { id: 2, src: brown, alt: "밤색도깨비" },
+    { id: 3, src: yellow, alt: "노랑도깨비" },
+    { id: 4, src: pink, alt: "핑크도깨비" },
+    { id: 5, src: orange, alt: "오렌지도깨비" },
+    { id: 6, src: green, alt: "초록도깨비" },
+  ];
+
+  // imageId를 받아 해당하는 이미지 객체를 반환하는 함수
+  const findImageById = (imageId) => images.find(image => image.id === imageId);
+
   useEffect(() => {
     const fetchUserEXP = async () => {
       try {
-        
         const res = await axios.get(`https://j10d202.p.ssafy.io/api/users/rank`, {
           headers: { Authorization: `Bearer ${accessToken}` },
         });
         console.log("리스폰스 확인 1 :", res)
         if (res.status === 200 && res.data.data) {
-          console.log("유저 id 확인", res.data.data.id);
-          console.log("유저 닉네임 확인", res.data.data.nickName);
-          console.log("유저 경험치 확인", res.data.data.exp);
-          console.log("유저 프로필 확인", res.data.data.imageId);
+          // console.log("유저 id 확인", res.data.data.id);
+          // console.log("유저 닉네임 확인", res.data.data.nickName);
+          // console.log("유저 경험치 확인", res.data.data.exp);
+          // console.log("유저 프로필 확인", res.data.data.imageId);
           setOtherUsers(res.data.data);
         } else {
           throw new Error('에러 발생 1');
@@ -77,16 +94,19 @@ export default function Ranking() {
 
       <h1 className={`text-center font-Bit text-6xl mb-4 ${styles.rankHeader}`}>순 위</h1>
     
-      <div className="flex flex-row justify-center items-center overflow-auto h-full">
-        <div className="flex flex-col items-center justify-center w-full">
-          {otherUsers.map((user, index) => (
-            <div key={user.id || index} className="flex items-center justify-between p-2 border-b border-gray-200 w-full">
-             <span>{index + 1}위</span>
-             <img src={user.imageId} alt="유저 프로필" className="h-10 w-10 rounded-full" />
-             <span>{user.nickName}</span>
-             <span>{user.exp.toLocaleString()}원</span>
-           </div>
-          ))}
+      <div className="flex flex-row justify-center items-center overflow-auto">
+        <div className="flex flex-col items-center justify-center w-1/2">
+          {otherUsers.map((user, index) => {
+            const image = findImageById(user.imageId); // 유저의 imageId에 해당하는 이미지 객체 찾기
+            return (
+              <div key={user.id || index} className="flex items-center justify-between p-2 border-b border-gray-200 w-full">
+                <span>{index + 1}위</span>
+                {image && <img src={image.src} alt={image.alt} className="h-10 w-10" />}
+                <span>{user.nickName}</span>
+                <span>{user.exp.toLocaleString()}원</span>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
