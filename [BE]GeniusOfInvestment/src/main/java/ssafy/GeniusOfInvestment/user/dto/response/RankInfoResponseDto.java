dc45446312440fd2e4751ceef4114020f5bfe5d5
wnstj7866 <wnstj7866@naver.com>
2024-03-21 17:39:39 +0900

    Feat : RankInfoResponseDto imageId 필드 추가

--- [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/user/dto/response/RankInfoResponseDto.java
+++ [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/user/dto/response/RankInfoResponseDto.java
@@ -2,20 +2,23 @@ package ssafy.GeniusOfInvestment.user.dto.response;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import ssafy.GeniusOfInvestment._common.entity.User;
 
 @Getter
 public class RankInfoResponseDto {
 
-    private Long id;
-    private String nickName;
-    private Long exp;
+    private final Long id;
+    private final String nickName;
+    private final Long exp;
+    private final int imageId;
 
     @Builder
-    private RankInfoResponseDto(Long id, String nickName, Long exp){
+    private RankInfoResponseDto(Long id, String nickName, Long exp,int imageId){
         this.id = id;
         this.nickName = nickName;
         this.exp = exp;
+        this.imageId = imageId;
     }
 
     public static RankInfoResponseDto from(User user){
@@ -23,8 +26,7 @@ public class RankInfoResponseDto {
                 .id(user.getId())
                 .nickName(user.getNickName())
                 .exp(user.getExp())
+                .imageId(user.getImageId())
                 .build();
     }
-
-
 }
