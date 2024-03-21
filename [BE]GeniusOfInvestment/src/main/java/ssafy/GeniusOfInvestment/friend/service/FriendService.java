a7e0aa65e78583b075f38e2b4e809cbae2e68b08
이준서 <wnstj7866@naver.com>
2024-03-22 02:03:59 +0900

    Feat : Alarm, Friend 메서드 추가

--- [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/friend/service/FriendService.java
+++ [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/friend/service/FriendService.java
@@ -1,20 +1,44 @@
 package ssafy.GeniusOfInvestment.friend.service;
 
 import java.util.List;
+import java.util.Optional;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ssafy.GeniusOfInvestment._common.entity.Friend;
+import ssafy.GeniusOfInvestment._common.entity.User;
+import ssafy.GeniusOfInvestment._common.exception.CustomBadRequestException;
+import ssafy.GeniusOfInvestment._common.response.ErrorType;
+import ssafy.GeniusOfInvestment.friend.dto.request.DeleteFriendRequest;
 import ssafy.GeniusOfInvestment.friend.dto.response.FriendListResponse;
 import ssafy.GeniusOfInvestment.friend.repository.FriendRepository;
+import ssafy.GeniusOfInvestment.user.repository.UserRepository;
 
 @Service
 @RequiredArgsConstructor
 public class FriendService {
 
     private final FriendRepository friendRepository;
+    private final UserRepository userRepository;
     public List<FriendListResponse> getFriendList(Long userId) {
 
         List<Friend> list = friendRepository.findFriendByUserIdOrFriendId(userId);
         return list.stream().map(FriendListResponse::from).toList();
     }
+
+    @Transactional
+    public void deleteFriend(Long userId, DeleteFriendRequest deleteFriendRequest) {
+        Long friendListId = deleteFriendRequest.getFriendListId();
+        Optional<Friend> friendInfo = friendRepository.findById(friendListId);
+        if(friendInfo.isEmpty()){
+            throw new CustomBadRequestException(ErrorType.NOT_FOUND_FRIEND);
+        }
+        Friend friend = friendInfo.get();
+        if(!friend.getUser().getId().equals(userId) && !friend.getFriend().getId().equals(userId)){
+            throw new CustomBadRequestException(ErrorType.NOT_VALID_FRIEND_NUM);
+        }
+        friendRepository.delete(friend);
+    }
+
 }
