bc3293deb59eb2212ae92227bef3285a122d3f84
jungminho <allmin9702@naver.com>
2024-03-22 23:58:47 +0900

    Fix : 에ã러코ë드 수정

--- [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/_common/response/ErrorType.java
+++ [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/_common/response/ErrorType.java
@@ -31,12 +31,12 @@ public enum ErrorType {
     //******************** Square & Room **************************//
     ROOM_NOT_FOUND(HttpStatus.NOT_FOUND,"입장하려는 방이 존재하지 않습니다."),
     IS_NOT_AVAILABLE_REDISUSER(HttpStatus.I_AM_A_TEAPOT,"유저동선 추적오류 입니다."),
-    NOT_AVAILABLE_CHANNEL(HttpStatus.BAD_REQUEST,"해당채널이 존재하지 않습니다."),
+    NOT_AVAILABLE_CHANNEL(HttpStatus.NOT_FOUND,"해당채널이 존재하지 않습니다."),
     CHANNEL_NOT_FOUND(HttpStatus.NOT_FOUND,"해당채널이 존재하지 않습니다."),
     NOT_FOUND_REDISUSER(HttpStatus.I_AM_A_TEAPOT,"유저동선 추적오류 입니다."),
 
     //******************** CHANNEL **************************//
-    CHANNEL_IS_FULL(HttpStatus.BAD_REQUEST,"입장하려는 채널이 가득 찼습니다."),
+    CHANNEL_IS_FULL(HttpStatus.NOT_FOUND,"입장하려는 채널이 가득 찼습니다."),
 
     //****************************Game****************************//
     IS_NOT_MANAGER(HttpStatus.UNAUTHORIZED, "방장만이 요청을 할 수 있습니다."),
