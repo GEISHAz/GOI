c077fbe0134b157db37f19f9a3ee7ea1989d6117
jungminho <allmin9702@naver.com>
2024-03-25 20:57:47 +0900

    Fix : nullpointerException 에러 해결

--- [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/channel/service/ChannelService.java
+++ [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/channel/service/ChannelService.java
@@ -52,7 +52,7 @@ public class ChannelService {
     //채널 들어가기
     public void enterChannel(User user, Long channelId) {
 
-        if(user.getChannel().getId().equals(channelId))
+        if(user.getChannel()!=null && user.getChannel().getId().equals(channelId))
             user.deleteChannel();
 
         log.info("유저 현 채널 : "+user.getChannel());
