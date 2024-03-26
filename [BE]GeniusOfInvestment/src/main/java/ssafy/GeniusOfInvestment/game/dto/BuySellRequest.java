3e04e092f1e58ed6dab04f6c1345997a218a0229
정원준 <museoul@naver.com>
2024-03-26 15:21:05 +0900

    Feat: 주식 매수,매도 기능 개발 완료

---
+++ [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/game/dto/BuySellRequest.java
@@ -0,0 +1,4 @@
+package ssafy.GeniusOfInvestment.game.dto;
+
+public record BuySellRequest(Long grId, String item, int share) {
+}
--- [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/game/service/StockService.java
+++ [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/game/service/StockService.java
@@ -148,6 +148,7 @@ public class StockService {
         GameMarket tmp = new GameMarket();
         tmp.setItem(item);
         int idx = room.getMarket().indexOf(tmp);
+        if(idx == -1) throw new CustomBadRequestException(ErrorType.NOT_FOUND_STOCK_ITEM);
         GameMarket gm = room.getMarket().get(idx);
         List<Long> history = gm.getCost();
         int curYear = room.getYear(); //현재 년도
@@ -162,5 +163,84 @@ public class StockService {
             year++;
         }
         return rst;
+    }
+
+    public MyTradingInfo buyStockItem(User user, Long grId, String item, int share){
+        GameRoom room = gameRepository.getOneGameRoom(grId);
+        if(room == null){
+            throw new CustomBadRequestException(ErrorType.NOT_FOUND_ROOM);
+        }
+
+        GameMarket tmp = new GameMarket();
+        tmp.setItem(item);
+        int idx = room.getMarket().indexOf(tmp);
+        if(idx == -1) throw new CustomBadRequestException(ErrorType.NOT_FOUND_STOCK_ITEM);
+        GameMarket gm = room.getMarket().get(idx); //사용자가 요청한 주식 정보를 찾는다.
+        int size = gm.getCost().size();
+        Long curCost = gm.getCost().get(size-1); //이 주식의 현재 한 주당 가격
+        MyTradingInfo mine = myTradingInfoRepository.getOneMyTradingInfo(user.getId());
+        if(mine == null) throw new CustomBadRequestException(ErrorType.NOT_FOUND_TRADINGINFO);
+        Long balance = mine.getRemainVal(); //현재 투자할 수 있는 잔고
+
+        if(balance < curCost * share){
+            throw new CustomBadRequestException(ErrorType.INSUFFICIENT_BALANCE);
+        }
+        BreakDown bdtmp = new BreakDown();
+        bdtmp.setItem(item);
+        idx = mine.getBreakDowns().indexOf(bdtmp); //내 거래내역에서 해당 주식 종목을 찾는다.
+        if(idx == -1) { //이 종목에 대해서 거래 내역이 없다.
+            mine.getBreakDowns().add(BreakDown.builder()
+                            .item(item)
+                            .buyVal(curCost)
+                            .shares(share)
+                            .nowVal(curCost)
+                            .roi(0)
+                    .build());
+        }else { //이 종목에 대해서 이미 거래 내역이 있다.
+            BreakDown bd = mine.getBreakDowns().get(idx);
+            Long lastTotal = bd.getBuyVal() * bd.getShares();
+            Long myAvg = (lastTotal + (curCost*share)) / (share + bd.getShares());
+            bd.setBuyVal(myAvg);
+            bd.setShares(share + bd.getShares());
+            //mine.getBreakDowns().set(idx, bd); //없어도 되지만 코드 가독성을 위해 추가??
+        }
+        mine.setInvestVal(mine.getInvestVal() + (curCost * share)); //이전 투자금액 + 이번에 투자한 주식 금액
+        mine.setRemainVal(balance - (curCost * share)); //잔고에서 이번에 투자한 금액 빼기
+        myTradingInfoRepository.updateMyTradingInfo(mine);
+        return mine;
+    }
+
+    public MyTradingInfo sellStockItem(User user, Long grId, String item, int share){
+        GameRoom room = gameRepository.getOneGameRoom(grId);
+        if(room == null){
+            throw new CustomBadRequestException(ErrorType.NOT_FOUND_ROOM);
+        }
+
+        GameMarket tmp = new GameMarket();
+        tmp.setItem(item);
+        int idx = room.getMarket().indexOf(tmp);
+        if(idx == -1) throw new CustomBadRequestException(ErrorType.NOT_FOUND_STOCK_ITEM);
+        GameMarket gm = room.getMarket().get(idx); //사용자가 요청한 주식 정보를 찾는다.
+        int size = gm.getCost().size();
+        Long curCost = gm.getCost().get(size-1); //이 주식의 현재 한 주당 가격
+        MyTradingInfo mine = myTradingInfoRepository.getOneMyTradingInfo(user.getId());
+        if(mine == null) throw new CustomBadRequestException(ErrorType.NOT_FOUND_TRADINGINFO);
+
+        BreakDown bdtmp = new BreakDown();
+        bdtmp.setItem(item);
+        idx = mine.getBreakDowns().indexOf(bdtmp); //내 거래내역에서 해당 주식 종목을 찾는다.
+        if(idx == -1) throw new CustomBadRequestException(ErrorType.NOT_FOUND_TRADINGINFO);
+
+        BreakDown bd = mine.getBreakDowns().get(idx);
+        int remainShare = bd.getShares() - share; //팔고 남은 주식 수
+        if(remainShare == 0){ //이 종목에서 내가 가지고 있는 주식을 모두 다 판다.
+            mine.getBreakDowns().remove(idx);
+        }else {
+            bd.setShares(remainShare);
+        }
+        mine.setInvestVal(mine.getInvestVal() - (curCost * share)); //이전 투자금액 - 이번에 매도한 주식 금액
+        mine.setRemainVal(mine.getRemainVal() + (curCost * share)); //잔고에서 이번에 매도한 금액 더하기
+        myTradingInfoRepository.updateMyTradingInfo(mine);
+        return mine;
     }
 }
