7727304284cedac19a529592a1ffac75138d7cd5
wnstj7866 <wnstj7866@naver.com>
2024-03-26 16:17:53 +0900

    Feat : 나와 상대방이 친구가 되어 있는지 확인하는 메서드 추가

--- [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/friend/repository/FriendRepository.java
+++ [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/friend/repository/FriendRepository.java
@@ -1,15 +1,20 @@
 package ssafy.GeniusOfInvestment.friend.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ssafy.GeniusOfInvestment._common.entity.Friend;
+import ssafy.GeniusOfInvestment._common.entity.User;
 
 public interface FriendRepository extends JpaRepository<Friend, Long> {
 
     @Query("SELECT f FROM Friend f WHERE f.user.id = :userId OR f.friend.id = :userId")
     List<Friend> findFriendByUserIdOrFriendId(Long userId);
 
+    @Query("SELECT f FROM Friend f " +
+            "WHERE (f.user = :userId AND f.friend = :fromId) " +
+            "OR (f.user = :fromId AND f.friend = :userId)")
+    Optional<Friend> findFriendsByUserAndFriend(User userId, User fromId);
 
-//    findBychatRoomId(Long chatRoomId);
 }
