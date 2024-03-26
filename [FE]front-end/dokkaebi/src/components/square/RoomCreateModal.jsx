38567d5fd824ebf0d68a0ac9bf3ebbaf27b57700
SUSIE <pyunsusie@naver.com>
2024-03-26 17:06:54 +0900

    Fix: 방 목록 요청 로직 수정

--- [FE]front-end/dokkaebi/src/components/square/RoomCreateModal.jsx
+++ [FE]front-end/dokkaebi/src/components/square/RoomCreateModal.jsx
@@ -78,7 +78,7 @@ export default function RoomCreateModal({ onClose, userName }) {
         if (response.status === 200 || response.status === 201) {
           console.log('방 생성 성공:', response);
           // 방 생성 성공하면 -> 생성한 방으로 이동
-          navigate(`/room/${response.roomnum}`);
+          navigate(`/room/${response.data.roomnum}`);
         }
       } catch (error) {
 
