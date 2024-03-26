050a562da7ef1a49dc1387bdb784936be79b1a98
SUSIE <pyunsusie@naver.com>
2024-03-26 20:59:08 +0900

    Fix: 채널 로직 수정

--- [FE]front-end/dokkaebi/src/components/channel/channel.jsx
+++ [FE]front-end/dokkaebi/src/components/channel/channel.jsx
@@ -40,9 +40,10 @@ export default function Channel() {
           headers: { Authorization: `Bearer ${accessToken}` },
         });
         // console.log("GET 리스폰스 확인 :", res)
-        if (res.status === 200 && res.data) {
+        if (res.status === 200 && res.data && res.msg) {
           // 받아오는 res.data 확인 -> id, channelName, userCount
-          console.log(res.data)
+          console.log("채널 백에서 온 데이터 확인", res.data)
+          console.log("msg 확인 :", res.msg)
           setGetChannelInfo(res.data)
           // const channelId = res.data
         } else {
