87a1f3d9fb61f88daaa6bab50672bdd5bcc910eb
SUSIE <pyunsusie@naver.com>
2024-03-30 00:28:12 +0900

    FiX: 로비 PlayerList 수정

--- [FE]front-end/dokkaebi/src/components/square/UserList.jsx
+++ [FE]front-end/dokkaebi/src/components/square/UserList.jsx
@@ -115,7 +115,7 @@ export default function UserList() {
               );
             })}
 
-          <div className="flex items-center justify-between p-2 border-b border-gray-200">
+          {/* <div className="flex items-center justify-between p-2 border-b border-gray-200">
             프로필 닉네임6글자 게임중 
           </div>
           <div className="flex items-center justify-between p-2 border-b border-gray-200">
@@ -168,7 +168,7 @@ export default function UserList() {
           </div>
           <div className="flex items-center justify-between p-2 border-b border-gray-200">
             더미데이터
-          </div>
+          </div> */}
 
         </div>
       </div>
