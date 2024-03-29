e3b23f74f990cf65ec0288fbdd18c0632bd2f27e
정원준 <museoul@naver.com>
2024-03-29 12:24:33 +0900

    Fix: 웹소켓 핸들러 로그 추가

--- [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/_common/utils/StompHandler.java
+++ [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/_common/utils/StompHandler.java
@@ -10,6 +10,7 @@ import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
+import ssafy.GeniusOfInvestment._common.jwt.JwtUtil;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,7 +18,7 @@ import org.springframework.stereotype.Component;
 @Order(Ordered.HIGHEST_PRECEDENCE + 99)
 public class StompHandler implements ChannelInterceptor {
 
-    //private final JWTUtil jwtUtil;
+    private final JwtUtil jwtUtil;
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel){
@@ -29,8 +30,21 @@ public class StompHandler implements ChannelInterceptor {
 //            if(checkToken != JwtCode.ACCESS){
 //                throw new InvalidTokenException("유효한 토큰이 아닙니다.");
 //            }
-        }
+            String token = String.valueOf(accessor.getNativeHeader("Authorization").get(0));
+            token = token.replace("Bearer ", "");
+            log.info("presend에서 웹소켓 통신시 받아온 token 값: " + token);
 
+            try {
+                String userId = jwtUtil.getUserId(token);
+                log.info("웹소켓 통신시 추출한 유저 아이디: " + userId);
+                accessor.addNativeHeader("User", userId);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+//            catch (JWTVerificationException e) {
+//                e.printStackTrace();
+//            }
+        }
         return message;
     }
 }
--- [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/square_room/controller/RoomChatController.java
+++ [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/square_room/controller/RoomChatController.java
@@ -11,17 +11,29 @@ import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import ssafy.GeniusOfInvestment.square_room.dto.RoomChatMessageDto;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Slf4j
 @RequiredArgsConstructor
 @Controller
 public class RoomChatController {
 
     private final SimpMessageSendingOperations messageSendingOperations;
+    private Map<String, String> sessions = new HashMap<>();
 
     // 새로운 사용자가 웹 소켓을 연결할 때 실행됨
     // @EventListener은 한개의 매개변수만 가질 수 있다.
     @EventListener(SessionConnectEvent.class)
     public void handleWebSocketConnectListener(SessionConnectEvent event) {
+        String sessionId = event.getMessage().getHeaders().get("simpSessionId").toString();
+        log.info("웹소켓 연결시 Event에서 sessionId: " + sessionId);
+//        StompHeaderAccessor headerAccesor = StompHeaderAccessor.wrap(event.getMessage());
+//        String sessionId = headerAccesor.getSessionId();
+        String userId = event.getMessage().getHeaders().get("nativeHeaders").toString().split("User=\\[")[1].split("]")[0];
+        log.info("웹소켓 연결시 Event에서 userId: " + userId);
+
+        sessions.put(sessionId, userId);
         log.info("Received a new web socket connection");
     }
     @EventListener(SessionDisconnectEvent.class)
