c75098d495b2486261730a815668bf8e6fc7c2be
jungminho <allmin9702@naver.com>
2024-03-31 17:43:34 +0900

    오Fix : 오류확인

--- [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/square_room/service/RoomService.java
+++ [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/square_room/service/RoomService.java
@@ -58,7 +58,7 @@ public class RoomService {
 
         if(room.getPassword() != null){ //방에 비밀번호가 설정되어 있다.
             if(!room.getPassword().equals(enterInfo.password())){
-                log.info("Room 비밀방 입장시 roomId"+enterInfo.roomId());
+                log.info("Room 비밀방 입장시 roomId"+enterInfo.roomNum());
                 throw new CustomRoomEnterException(ErrorType.INVALID_PASSWORD, enterInfo.roomId());
             }
         }
