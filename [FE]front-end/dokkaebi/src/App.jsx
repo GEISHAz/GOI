9ca22b662ed720ca12b30b1c860091227a420743
이귀현 <dlrnlgus7660@naver.com>
2024-04-02 17:53:40 +0900

    Feat: 사용자 새로고침 시 경고알림 설정

--- [FE]front-end/dokkaebi/src/App.jsx
+++ [FE]front-end/dokkaebi/src/App.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Router from './router.jsx';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const handleBeforeUnload = (e) => {
+      const message = '새로고침 시 문제가 발생할 수 있어요 ! 조심해주세요 !';
+      e.returnValue = message;
+      return message;
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
 
   return (
     <div className='font-Galmuri11'>
