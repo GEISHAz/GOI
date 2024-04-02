f4c101932314f023df97a5039bd0a454eade1081
wnstj7866 <wnstj7866@naver.com>
2024-04-02 13:26:04 +0900

    Feat : GameChatMessageDto receiver 필드 추가

--- [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/game/dto/GameChatMessageDto.java
+++ [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/game/dto/GameChatMessageDto.java
@@ -7,10 +7,11 @@ import lombok.Setter;
 @Setter
 public class GameChatMessageDto {
     public enum MessageType {
-        ENTER, TALK
+        ENTER, TALK, WHISPER
     }
     private MessageType type; // 메시지 타입
     private String roomId; // 방번호
     private String sender; // 메시지 보낸사람
     private String message; // 메시지
+    private String receiver; // 받는 사람 닉네임
 }
