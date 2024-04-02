5621e8ee3b6dd74c2fe1165438c432605bef905b
wnstj7866 <wnstj7866@naver.com>
2024-04-02 14:21:01 +0900

    Feat : GameChatController log 추가

--- [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/game/controller/GameChatController.java
+++ [BE]GeniusOfInvestment/src/main/java/ssafy/GeniusOfInvestment/game/controller/GameChatController.java
@@ -1,6 +1,7 @@
 package ssafy.GeniusOfInvestment.game.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
@@ -8,12 +9,14 @@ import ssafy.GeniusOfInvestment.game.dto.GameChatMessageDto;
 
 @RequiredArgsConstructor
 @Controller
+@Slf4j
 public class GameChatController {
 
     private final SimpMessageSendingOperations messageSendingOperations;
 
     @MessageMapping("/game/chat/message/")
     public void message(GameChatMessageDto gameChatMessageDto){
+        log.info("////////////////////////////////////////"+gameChatMessageDto.getReceiver());
         messageSendingOperations.convertAndSend("/sub/game/chat/" + gameChatMessageDto.getRoomId(),gameChatMessageDto);
     }
 }
